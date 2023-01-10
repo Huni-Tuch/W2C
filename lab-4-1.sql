0645f9f441dd1ae2d7a1b612dca4b12f649f68bb
Huni-Tuch <hunibert.tuch@kellogg.northwestern.edu>
2023-01-10 22:21:27 +0000

    Inner Join in class example

--- lab-4-1.sql
+++ lab-4-1.sql
@@ -1,5 +1,13 @@
 -- How many lifetime hits does Barry Bonds have?
 
+SELECT players.first_name, players.last_name, SUM(stats.hits)
+FROM stats
+INNER JOIN players ON stats.player_id = players.id 
+WHERE players.first_name = "Barry"
+    AND players.last_name = "Bonds"
+GROUP BY player_id
+;
+
 -- Expected result:
 -- 2935
 
